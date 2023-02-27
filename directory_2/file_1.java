134eff4ee2a9c6facbe40b9c2a0c991f7ff4bc5a
Punnam Aravind <punnam.aravind@ukg.com>
2023-02-27 20:57:33 +0530

    Added some raw code to the directory_2/file_1.java

--- directory_2/file_1.java
+++ directory_2/file_1.java
@@ -1,5 +1,10 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java in file 1");  
+      System.out.println("Hello Java in file 1");
+      System.out.println("Added new line");  
     }  
+
+    public newFunction(String args[]){
+      System.out.println("Added new function");      
+    }
 }  
